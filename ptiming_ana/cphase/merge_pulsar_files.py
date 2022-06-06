024ccd52139989ed54e7a0acd5d9efc44d5603d8
Alvaro Mas <66162723+alvmas@users.noreply.github.com>
2022-06-06 11:19:27 +0200

    Update merge_pulsar_files.py

--- ptiming_ana/cphase/merge_pulsar_files.py
+++ ptiming_ana/cphase/merge_pulsar_files.py
@@ -1,7 +1,7 @@
 #####################
 ###Author: Alvaro Mas Aguilar (alvmas)
 #mail: alvmas@ucm.es
-#Using modules from PINT-pulsar and lstchain to calculate phases and add them to the input files.
+#Using modules from lstchain and pandas to merge files
 ###################3
 
 
@@ -16,20 +16,20 @@ from .utils import merge_dl2_pulsar
     
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--dir', '-d', action='store',type=str,dest='directory',default=None)
-	parser.add_argument('--output','-out',action='store',type=str,dest='dir_output',default=None)
-	parser.add_argument('--srcdep','-srcdep',action='store',type=bool,dest='src_dep',default=False)
-	parser.add_argument('--run-number','-r',action='store',type=str,dest='run',default=False)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dir', '-d', action='store',type=str,dest='directory',default=None)
+    parser.add_argument('--output','-out',action='store',type=str,dest='dir_output',default=None)
+    parser.add_argument('--srcdep','-srcdep',action='store',type=bool,dest='src_dep',default=False)
+    parser.add_argument('--run-number','-r',action='store',type=str,dest='run',default=False)
     
     
-	args = parser.parse_args()
-	output_dir=args.dir_output
-	src_dep=args.src_dep
-	run=args.run
-        directory=args.directory
+    args = parser.parse_args()
+    output_dir=args.dir_output
+    src_dep=args.src_dep
+    run=args.run
+    directory=args.directory
 
-	merge_dl2_pulsar(directory,run,output_dir,src_dep)
+    merge_dl2_pulsar(directory,run,output_dir,src_dep)
 
 if __name__ == "__main__":
     main()
