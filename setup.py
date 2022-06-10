2d6fecca85350247be6b24bf5bf068c20fa5825b
Alvaro Mas <66162723+alvmas@users.noreply.github.com>
2022-06-10 09:36:17 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'scipy',
         'probfit',
         'tables',
-        'protobuf~=3.20.0',
+        'protobuf<=3.20.0',
         'toml',
         'pint-pulsar~=0.8.8',
         'setuptools_scm',
