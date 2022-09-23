bb3579b2e62764516994d28ed058aa56328de4ba
alvaro mas <alvaro.mas@cp01.ctan.cta-observatory.org>
2022-09-23 14:25:04 +0000

    add documentation to scripts

--- ptiming_ana/cphase/add_DL2_phase_table.py
+++ ptiming_ana/cphase/add_DL2_phase_table.py
@@ -5,6 +5,58 @@
 ###################3
 
 
+'''
+Script that adds the information of the phases and corrected times for the pulsar analysis to a DL2 file. It includes this information in a new table called 'phase_info' and optionally a new table with the ource position and theta2 values (called 'source_position')
+
+Parameters:
+----------------------
+--dir: string 
+  Directory where to find the standard DL3 files
+
+--in-file: string
+  DL3 standard file if want to analyze only one file
+
+--ephem:string
+  Path to the ephemeris file (.par or .gro)
+
+--run-number: int 
+  Run number to process (only if --dir is given)
+
+--include-theta: boolean
+  True if want to add the source position and theta2 values on the DL2 files (in a table called 'source_position')
+
+--interpolation: boolean 
+   Set to True if want to use the interpolation method (faster but loses some precision)
+
+
+Usage:
+------------------------
+1. An example of usage for a given file is:
+python add_DL2_phase_table.py 
+       --in-file ./DL2_directory/dl2_LST-1.Run0000.fits.gz 
+       --ephem model_test.par
+
+2. Another way of calling (if we have a directory and the run number) is:
+python add_DL2_phase_table.py 
+       --dir ./DL2_directory/
+       --run-number 0000
+       --ephem model_test.par
+
+3. If we want to use the interpolation method:
+python add_DL2_phase_table.py 
+       --in_file ./DL2_directory/dl3_LST-1.Run0000.fits.gz 
+       --ephem model_test.par
+       --interpolation
+
+4. If we want to include the source position and the theta2 values:
+python add_DL2_phase_table.py 
+       --in_file ./DL2_directory/dl3_LST-1.Run0000.fits.gz 
+       --ephem model_test.par
+       --interpolation
+       --include-theta
+
+'''
+
 import sys
 import pandas as pd
 import argparse
@@ -17,11 +69,11 @@ from utils import add_source_info_dl2
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dir', '-d', action='store',type=str,dest='directory',default=None)
-	parser.add_argument('--in_file', '-f', action='store',type=str,dest='in_file',default=None)
+	parser.add_argument('--in-file', '-f', action='store',type=str,dest='in_file',default=None)
 	parser.add_argument('--ephem','-ephem',action='store',type=str,dest='ephem',default=None)
 	parser.add_argument('--pickle','-pickle',action='store',type=bool,dest='pickle',default=False)
-	parser.add_argument('--run_number','-r',action='store',type=str,dest='run',default=False)
-	parser.add_argument('--include_theta','-t',action='store_true',dest='include_theta')
+	parser.add_argument('--run-number','-r',action='store',type=str,dest='run',default=False)
+	parser.add_argument('--include-theta','-t',action='store_true',dest='include_theta')
 	parser.add_argument('--interpolation','-interp',action='store_true',dest='interpolation')
     
     
--- ptiming_ana/cphase/add_DL3_phase_table.py
+++ ptiming_ana/cphase/add_DL3_phase_table.py
@@ -4,6 +4,52 @@
 #Using modules from PINT-pulsar and lstchain to calculate phases and add them to the input files.
 ###################3
 
+'''
+Script that creates from a DL3 standard file a DL3_pulsar file with the information of the phases and corrected times for the pulsar analysis. 
+
+Parameters:
+----------------------
+--dir: string 
+  Directory where to find the standard DL3 files
+
+--in-file: string
+  DL3 standard file if want to analyze only one file
+
+--ephem:string
+  Path to the ephemeris file (.par or .gro)
+
+--output:string
+  Path where to store the DL3 file with the pulsar info
+
+--run-number: int 
+  Run number to process (only if --dir is given)
+
+--interpolation: boolean 
+   Set to True if want to use the interpolation method (faster but loses some precision)
+
+Usage:
+------------------------
+1. An example of usage for a given file is:
+python add_DL3_phase_table.py 
+       --in-file ./DL3_directory/dl3_LST-1.Run0000.fits.gz 
+       --output ./DL3_pulsar_directory/ 
+       --ephem model_test.par
+
+2. Another way of calling (if we have a directory and the run number) is:
+python add_DL3_phase_table.py 
+       --dir ./DL3_directory/
+       --run-number 0000
+       --output ./DL3_pulsar_directory/ 
+       --ephem model_test.par
+
+3. If we want to use the interpolation method:
+python add_DL3_phase_table.py 
+       --in_file ./DL3_directory/dl3_LST-1.Run0000.fits.gz 
+       --output ./DL3_pulsar_directory/ 
+       --ephem model_test.par
+       --interpolation
+
+'''
 
 import sys
 import pandas as pd
@@ -17,11 +63,11 @@ from ptiming_ana.cphase.pulsarphase_cal import DL3_calphase
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--dir', '-d', action='store',type=str,dest='directory',default=None)
-	parser.add_argument('--in_file', '-f', action='store',type=str,dest='in_file',default=None)
+	parser.add_argument('--in-file', '-f', action='store',type=str,dest='in_file',default=None)
 	parser.add_argument('--ephem','-ephem',action='store',type=str,dest='ephem',default=None)
 	parser.add_argument('--output','-out',action='store',type=str,dest='dir_output',default=None)
 	parser.add_argument('--pickle','-pickle',action='store',type=bool,dest='pickle',default=False)
-	parser.add_argument('--run_number','-r',action='store',type=str,dest='run',default=False)
+	parser.add_argument('--run-number','-r',action='store',type=str,dest='run',default=False)
 	parser.add_argument('--interpolation','-interp',action='store_true',dest='interpolation')
     
     
